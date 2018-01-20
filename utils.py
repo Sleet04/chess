a043fa8cb6114d19ab4866e611343e6e21a2f524
sleet <paul.chambon04@gmail.com>
2018-01-20 14:03:15 +0100

    probleme instantiation réglé

--- utils.py
+++ utils.py
@@ -21,7 +21,7 @@ class Utils :
 		# 	writer = csv.writer(logFile)
 		# 	[writer.writerow(r) for r in board]
 
-	def dwnImg() :
+	def dwnImg(self) :
 		pieces = {
 			'white_queen' : pygame.image.load('img/white_queen.png'),
 			'white_king' : pygame.image.load('img/white_king.png'),
@@ -36,13 +36,14 @@ class Utils :
 			'black_pawn' : pygame.image.load('img/black_pawn.png'),
 			'black_knight' : pygame.image.load('img/black_knight.png')
 		}
+		return pieces
 
-	def createText(text,x,y) :
+	def createText(self,text,x,y) :
 		fontObj = pygame.font.Font('freesansbold.ttf', 32)
-		textSurfaceObj = fontObj.render(text,True,BLACK)
+		textSurfaceObj = fontObj.render(text,True,self.BLACK)
 		textRectObj = textSurfaceObj.get_rect()
 		textRectObj.center = (x,y)
-		DISPLAYSURF.blit(textSurfaceObj,textRectObj)
+		self.DISPLAYSURF.blit(textSurfaceObj,textRectObj)
 
 
 	def setBoard (self,DISPLAYSURF,menuWidth,TOTALWIDTH,TOTALHEIGHT,cellWidth,cellInfoWidth,cellLineWidth,cellHeight) : 
@@ -99,30 +100,30 @@ class Utils :
 				else :
 					background = 1
 
-				boardCase[cellLetter+cellNumber] = pygame.draw.rect(DISPLAYSURF, BLACK, (i,j, cellWidth, cellHeight),background)
+				boardCase[cellLetter+cellNumber] = pygame.draw.rect(DISPLAYSURF, self.BLACK, (i,j, cellWidth, cellHeight),background)
 		 #create the coordiantes (a1,h8 etc ...)
 		for i in range(0,8*9,9) :
 			if i == 0 :
-				createText('8',boardCase['A8'].left+(TOTALWIDTH-menuWidth-(cellInfoWidth/2)),boardCase['A8'].height/2)
-				createText('A',boardCase['A8'].width/2+menuWidth,boardCase['A8'].top+(TOTALHEIGHT-(cellInfoWidth/2)))
+				self.createText('8',boardCase['A8'].left+(TOTALWIDTH-menuWidth-(cellInfoWidth/2)),boardCase['A8'].height/2)
+				self.createText('A',boardCase['A8'].width/2+menuWidth,boardCase['A8'].top+(TOTALHEIGHT-(cellInfoWidth/2)))
 			if i == 9 :
-				createText('7',boardCase['B7'].left+(TOTALWIDTH-menuWidth-(cellInfoWidth/2))-cellWidth,boardCase['B7'].height/2+boardCase['B7'].top)
-				createText('B',boardCase['B7'].width/2+boardCase['B7'].left,boardCase['B7'].top+(TOTALHEIGHT-(cellInfoWidth/2))-boardCase['B7'].top)
+				self.createText('7',boardCase['B7'].left+(TOTALWIDTH-menuWidth-(cellInfoWidth/2))-cellWidth,boardCase['B7'].height/2+boardCase['B7'].top)
+				self.createText('B',boardCase['B7'].width/2+boardCase['B7'].left,boardCase['B7'].top+(TOTALHEIGHT-(cellInfoWidth/2))-boardCase['B7'].top)
 			if i == 18 :
-				createText('6',boardCase['C6'].left+(TOTALWIDTH-menuWidth-(cellInfoWidth/2))-cellWidth*2,boardCase['C6'].height/2+boardCase['C6'].top)
-				createText('C',boardCase['C6'].width/2+boardCase['C6'].left,boardCase['C6'].top+(TOTALHEIGHT-(cellInfoWidth/2))-cellHeight*2)
+				self.createText('6',boardCase['C6'].left+(TOTALWIDTH-menuWidth-(cellInfoWidth/2))-cellWidth*2,boardCase['C6'].height/2+boardCase['C6'].top)
+				self.createText('C',boardCase['C6'].width/2+boardCase['C6'].left,boardCase['C6'].top+(TOTALHEIGHT-(cellInfoWidth/2))-cellHeight*2)
 			if i == 27 :
-				createText('5',boardCase['D5'].left+(TOTALWIDTH-menuWidth-(cellInfoWidth/2))-cellWidth*3,boardCase['D5'].height/2+boardCase['D5'].top)
-				createText('D',boardCase['D5'].width/2+boardCase['D5'].left,boardCase['D5'].top+(TOTALHEIGHT-(cellInfoWidth/2))-cellHeight*3)
+				self.createText('5',boardCase['D5'].left+(TOTALWIDTH-menuWidth-(cellInfoWidth/2))-cellWidth*3,boardCase['D5'].height/2+boardCase['D5'].top)
+				self.createText('D',boardCase['D5'].width/2+boardCase['D5'].left,boardCase['D5'].top+(TOTALHEIGHT-(cellInfoWidth/2))-cellHeight*3)
 			if i == 36 :
-				createText('4',boardCase['E4'].left+(TOTALWIDTH-menuWidth-(cellInfoWidth/2))-cellWidth*4,boardCase['E4'].height/2+boardCase['E4'].top)
-				createText('E',boardCase['E4'].width/2+boardCase['E4'].left,boardCase['E4'].top+(TOTALHEIGHT-(cellInfoWidth/2))-cellHeight*4)
+				self.createText('4',boardCase['E4'].left+(TOTALWIDTH-menuWidth-(cellInfoWidth/2))-cellWidth*4,boardCase['E4'].height/2+boardCase['E4'].top)
+				self.createText('E',boardCase['E4'].width/2+boardCase['E4'].left,boardCase['E4'].top+(TOTALHEIGHT-(cellInfoWidth/2))-cellHeight*4)
 			if i == 45 :
-				createText('3',boardCase['F3'].left+(TOTALWIDTH-menuWidth-(cellInfoWidth/2))-cellWidth*5,boardCase['F3'].height/2+boardCase['F3'].top)
-				createText('F',boardCase['F3'].width/2+boardCase['F3'].left,boardCase['F3'].top+(TOTALHEIGHT-(cellInfoWidth/2))-cellHeight*5)
+				self.createText('3',boardCase['F3'].left+(TOTALWIDTH-menuWidth-(cellInfoWidth/2))-cellWidth*5,boardCase['F3'].height/2+boardCase['F3'].top)
+				self.createText('F',boardCase['F3'].width/2+boardCase['F3'].left,boardCase['F3'].top+(TOTALHEIGHT-(cellInfoWidth/2))-cellHeight*5)
 			if i == 54 :
-				createText('2',boardCase['G2'].left+(TOTALWIDTH-menuWidth-(cellInfoWidth/2))-cellWidth*6,boardCase['G2'].height/2+boardCase['G2'].top)
-				createText('G',boardCase['G2'].width/2+boardCase['G2'].left,boardCase['G2'].top+(TOTALHEIGHT-(cellInfoWidth/2))-cellHeight*6)
+				self.createText('2',boardCase['G2'].left+(TOTALWIDTH-menuWidth-(cellInfoWidth/2))-cellWidth*6,boardCase['G2'].height/2+boardCase['G2'].top)
+				self.createText('G',boardCase['G2'].width/2+boardCase['G2'].left,boardCase['G2'].top+(TOTALHEIGHT-(cellInfoWidth/2))-cellHeight*6)
 			if i == 63 :
-				createText('1',boardCase['H1'].left+(TOTALWIDTH-menuWidth-(cellInfoWidth/2))-cellWidth*7,boardCase['H1'].height/2+boardCase['H1'].top)
-				createText('H',boardCase['H1'].width/2+boardCase['H1'].left,boardCase['H1'].top+(TOTALHEIGHT-(cellInfoWidth/2))-cellHeight*7)
+				self.createText('1',boardCase['H1'].left+(TOTALWIDTH-menuWidth-(cellInfoWidth/2))-cellWidth*7,boardCase['H1'].height/2+boardCase['H1'].top)
+				self.createText('H',boardCase['H1'].width/2+boardCase['H1'].left,boardCase['H1'].top+(TOTALHEIGHT-(cellInfoWidth/2))-cellHeight*7)
